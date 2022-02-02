e5639de1b25064b0f97fdae585c0bf1438d267cb
lukegjk <gajdzik.luk@gmail.com>
2022-02-02 20:12:27 +0100

    first_tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        # Title should contain 'Lists', if not: test will fail
+        self.assertIn('Lists', self.browser.title)
+        self.fail('End of test.')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
